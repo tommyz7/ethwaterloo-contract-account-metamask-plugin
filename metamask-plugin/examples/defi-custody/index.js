44ee672cc2ba902f25732de6f6fbeb106d699b71
Tomasz Zacharczuk <tomasz.zacharczuk@gmail.com>
2019-11-09 08:55:25 -0500

    fix build path

--- metamask-plugin/examples/defi-custody/index.js
+++ metamask-plugin/examples/defi-custody/index.js
@@ -1,19 +1,23 @@
 const { errors: rpcErrors } = require('eth-json-rpc-errors')
-const ethereumjs = require('ethereumjs-wallet')
-const EthereumTx = require('ethereumjs-tx').Transaction
 
 const accounts = [];
+
+// ethers.js object
+let ethersWallet, contract;
 updateUi();
 
 wallet.registerRpcMessageHandler(async (_origin, req) => {
-  console.log('addAccount')
+  console.log('registerRpcMessageHandler origin, req', origin, req)
   switch (req.method) {
     case 'addAccount':
       addAccount(req.params);
       break;
 
+    case 'setLabel':
+      await setLabel(req.params);
+      break;
+
     default:
-      console.log('addAccount error')
       throw rpcErrors.methodNotFound(req)
   }
 
@@ -22,81 +26,82 @@ wallet.registerRpcMessageHandler(async (_origin, req) => {
 })
 
 wallet.registerAccountMessageHandler(async (origin, req) => {
+  console.log('registerAccountMessageHandler origin, req', origin, req)
   switch (req.method) {
+    // case 'eth_sendRawTransaction':
+    //   console.log('eth_sendRawTransaction origin, req', origin, req)
+    //   break;
+
     case 'eth_sign':
     case 'eth_signTransaction':
     case 'personal_sign':
     case 'wallet_signTypedData':
     case 'wallet_signTypedData_v3':
     case 'wallet_signTypedData_v4':
+      let result;
       console.log('origin, req', origin, req)
+      
+      let rawTxData = req.params[0];
+      console.log('rawTxData.data', rawTxData.data, rawTxData.data == "0x")
+      if (rawTxData.data == "0x") { // ETH transfer
+        console.log('contract', contract)
+        let build = require('./Word.json');
+        let iface = new ethers.utils.Interface(build.abi)
+        let calldata = iface.functions.sendEth.encode([rawTxData.to, rawTxData.value]);
+        console.log('calldata', calldata);
 
-      let ethjsWallet = await ethereumjs.fromPrivateKey(await wallet.getAppKey());
-      console.log('ethjsWallet', ethjsWallet);
-      console.log('getAddress()', '0x372314Cb378A43486FA9D25e0155AA2892fD3941', await walletWithProvider.getAddress());
+        let nonce = await ethersWallet.getTransactionCount()
+        console.log('nonce', nonce)
 
-      let addr = await ethjsWallet.getAddress()
-      let params = [
-        addr,
-        "latest"
-      ];
-
-      // let nonce = await wallet.send({
-      //   method: 'eth_getTransactionCount',
-      //   params: params,
-      //   from: addr
-      // })
-
-      console.log('nonce', nonce);
-
-      // Number()
-
-      // let transaction = {
-      //     nonce: await walletWithProvider.getTransactionCount(),
-      //     gasLimit: 221000,
-      //     gasPrice: req.params[0].gasPrice,
-
-      //     to: req.params[0].to,
-      //     // ... or supports ENS names
-      //     // to: "ricmoo.firefly.eth",
-
-      //     value: req.params[0].value,
-      //     data: req.params[0].data,
-      //     chainId: ethers.utils.getNetwork().chainId
-      // }
-
-      // let singedTx = await walletWithProvider.sign(transaction);
-      // console.log('wallet', wallet)
-
-      const result = await prompt({ customHtml: `<div style="width: 100%;overflow-wrap: break-word;">
+        // All properties are optional
+        let transaction = {
+            nonce: nonce,
+            gasLimit: rawTxData.gasLimit * 2,
+            gasPrice: rawTxData.gasPrice,
+            to: contract.address,
+            value: 0,
+            data: calldata
+            // This ensures the transaction cannot be replayed on different networks
+            // chainId: ethers.utils.getNetwork('homestead').chainId
+        }
+        console.log('transaction', transaction)
+        result = await ethersWallet.sign(transaction)
+      } else {
+        result = await prompt({ customHtml: `<div style="width: 100%;overflow-wrap: break-word;">
         The site from <span style="font-weight: 900;color: #037DD6;"><a href="${origin}">${origin}</a></span> requests you sign this with your offline strategy:\n${JSON.stringify(req)}
         </div>`})
-      return 0
+      }
+      console.log('result', result);
+      return result
     default:
       throw rpcErrors.methodNotFound(req)
   }
 })
 
 async function addAccount (params) {
-  validate(params);
-  const account = params[0]
+  console.log('params', params)
+  let provider = new ethers.providers.Web3Provider(wallet);
+  ethersWallet = new ethers.Wallet(await wallet.getAppKey(), provider);
+  console.log('ethersWallet.address', ethersWallet.address)
+  console.log('ethersWallet.getBalance()', await ethersWallet.getBalance())
+  // const account = params[0]
+  const account = await deployContract(ethersWallet)
+  // validate(account);
   const approved = await confirm(`Do you want to add offline account ${account} to your wallet?`)
   if (!approved) {
     throw rpcErrors.userRejectedRequest()
   }
   accounts.push(account);
+  console.log('accounts', accounts)
   updateUi();
 }
 
-async function deposit (params) {
-  validate(params);
-  const account = params[0]
-  const approved = await confirm(`Do you want to add offline account ${account} to your wallet?`)
-  if (!approved) {
-    throw rpcErrors.userRejectedRequest()
-  }
-  accounts.push(account);
-  updateUi();
+async function setLabel(params) {
+  let res = await wallet.send({
+    method: 'setAccountLabel',
+    params: [ 'DC Wallet', {address: accounts[0]}]
+  })
+  console.log('setAccountLabel result', res)
 }
 
 function validate (params) {
@@ -114,6 +119,36 @@ async function prompt (message) {
   const response = await wallet.send({ method: 'prompt', params: [message] });
   return response.result;
 }
+
+// Deployment is asynchronous, so we use an async IIFE
+async function deployContract(walletObj) {
+    let build = require('../../../../truffle/build/contracts/Word.json');
+    console.log('build', build)
+
+    // Create an instance of a Contract Factory
+    let factory = new ethers.ContractFactory(build.abi, build.bytecode, walletObj);
+
+    // Notice we pass in "Hello World" as the parameter to the constructor
+    contract = await factory.deploy("Hello World");
+
+    // The address the Contract WILL have once mined
+    // See: https://ropsten.etherscan.io/address/0x2bd9aaa2953f988153c8629926d22a6a5f69b14e
+    console.log('contract.address', contract.address);
+    // "0x2bD9aAa2953F988153c8629926D22A6a5F69b14E"
+
+    // The transaction that was sent to the network to deploy the Contract
+    // See: https://ropsten.etherscan.io/tx/0x159b76843662a15bd67e482dcfbee55e8e44efad26c5a614245e12a00d4b1a51
+    console.log('contract.deployTransaction.hash', contract.deployTransaction.hash);
+    // "0x159b76843662a15bd67e482dcfbee55e8e44efad26c5a614245e12a00d4b1a51"
+
+    // The contract is NOT deployed yet; we must wait until it is mined
+    await contract.deployed()
+
+    // Done! The contract is deployed.
+    return contract.address
+};
+
+
 
 function updateUi () {
   console.log('updating UI with accounts', accounts)
