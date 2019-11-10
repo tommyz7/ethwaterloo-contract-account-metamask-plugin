7f2887714756d1c87d7ab535df697ad0a99e3f7c
Tomasz Zacharczuk <tomasz.zacharczuk@gmail.com>
2019-11-10 01:20:10 -0500

    update migrations

--- truffle/migrations/2_deploy_tokens.js
+++ truffle/migrations/2_deploy_tokens.js
@@ -2,7 +2,7 @@ const USDC = artifacts.require("USDC");
 const TCAD = artifacts.require("TCAD");
 
 module.exports = function(deployer) {
-  let addr = "0x203ddBe2D4925DD7fDeE0C5d7d7e195D71E0e821";
+  let addr = "0x2DE810c2C7177266DFBF4F9ed3C9e41726dB1262";
   let val = web3.utils.toWei("100", "ether");
   deployer.deploy(USDC, addr, val);
   deployer.deploy(TCAD, addr, val);
